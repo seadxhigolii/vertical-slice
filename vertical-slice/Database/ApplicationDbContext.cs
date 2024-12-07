ad872af1aed40c975dd86cbff66b2bef0bc774d3
Sead Xhigoli <sead.xhigoli1@gmail.com>
2024-12-07 01:23:30 +0100

    Add project files.

---
+++ vertical-slice/Database/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using vertical_slice.Entities;
+
+namespace vertical_slice.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<Article>(builder =>
+            //    builder.OwnsOne(a=> a.))
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Article> Articles { get; set; }
+    }
+}
